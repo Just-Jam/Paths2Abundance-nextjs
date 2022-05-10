b85784ed62e6a8f08fb247232d638c7034413645
Sam Lin <samlyc11@gmail.com>
2022-05-10 17:52:11 +0800

    Removed contracts data, edit hashconnect service

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,11 @@ import { supabase } from '../utils/client'
 import { HashConnect } from 'hashconnect'
 import { hethers } from '@hashgraph/hethers'
 import { useEffect, useState, useCallback } from 'react';
-
+import { 
+  ContractCallQuery, 
+  ContractExecuteTransaction,
+  ContractFunctionParameters
+} from '@hashgraph/sdk'
 
 export default function Home({ Organizations, Solutions, Projects }) {
 
@@ -35,46 +39,66 @@ export default function Home({ Organizations, Solutions, Projects }) {
   });
 
   const initHashconnect = async () => {
-    //first init and store the private for later
-    let initData = await hashconnect.init(appMetadata);
-    saveData.privateKey = initData.privKey;
+    if(!loadLocalData()){
+      //first init and store the private for later
+      let initData = await hashconnect.init(appMetadata);
+      saveData.privateKey = initData.privKey;
 
-    //then connect, storing the new topic for later
-    const state = await hashconnect.connect();
-    saveData.topic = state.topic;
-    
-    //generate a pairing string, which you can display and generate a QR code from
-    saveData.pairingString = hashconnect.generatePairingString(state, "testnet", true);
-    
-    //find any supported local wallets
-    hashconnect.findLocalWallets();
-
-    console.log("Pairing String: ", saveData.pairingString)
-    // console.log("Paired Account: ", saveData.pairedAccounts)
-    // console.log("Paired Walletdata: ", saveData.pairedWalletData)
-    // console.log("Topic: ", saveData.topic)
-
-    connectToWallet();
+      //then connect, storing the new topic for later
+      const state = await hashconnect.connect();
+      saveData.topic = state.topic;
+      
+      //generate a pairing string, which you can display and generate a QR code from
+      saveData.pairingString = hashconnect.generatePairingString(state, "testnet", true);
+      
+      //find any supported local wallets
+      hashconnect.findLocalWallets();
+    } else {
+      await hashconnect.init(appMetadata, saveData.privateKey);
+      await hashconnect.connect(saveData.topic, saveData.pairedWalletData);
+    }
+    setUpEvents();
   }
 
-  const connectToWallet = async () => {
+  const setUpEvents = async () => {
     hashconnect.pairingEvent.once((pairingData) => {
       //example
       pairingData.accountIds.forEach(id => {
           if(saveData.pairedAccounts.indexOf(id) == -1)
               saveData.pairedAccounts.push(id);
       })
+      saveData.pairedWalletData = pairingData;
+      saveDataInLocalStorage();
       console.log(pairingData)
       console.log(saveData.pairedAccounts)
     })
-    
-    
   }
 
+  const saveDataInLocalStorage = () => {
+    let data = JSON.stringify(saveData);
+    localStorage.setItem("hashconnectData", data)
+  }
+
+  const loadLocalData = () => {
+    let foundData = localStorgage.getItem("hashconnectData")
+    if(foundData){
+      this.saveData = JSON.parse(foundData);
+      console.log("Found local data", this.saveData)
+      return true;
+    }
+    else return false;
+  }
+
+
+  //Works
   const getBalance = async () => {
     let HashConnectProvider = hashconnect.getProvider("testnet", saveData.topic, saveData.pairedAccounts[0])
     let balance = await HashConnectProvider.getAccountBalance(saveData.pairedAccounts[0]);
     console.log(balance)
+  }
+
+  const mintOrgNFT = async () => {
+    //get bytecode of safeMint function
   }
 
   const addOrgFunction = async () => {
@@ -97,8 +121,7 @@ export default function Home({ Organizations, Solutions, Projects }) {
   }
 
   useEffect(() => {
-    let hethersProvider = hethers.getDefaultProvider('testnet');
-    console.log(hethersProvider);
+
   },[])
 
   return (
@@ -112,6 +135,7 @@ export default function Home({ Organizations, Solutions, Projects }) {
       </li>
       <button onClick={() => initHashconnect()}>Connect Wallet</button>
       <button onClick={() => getBalance()}>Get Balance</button>
+      <button onClick={() => mintOrgNFT()}>Mint OrganizationNFT *Only Sam's address</button>
       <div>
         <form onSubmit={() => addOrgFunction()}>
           <label>
