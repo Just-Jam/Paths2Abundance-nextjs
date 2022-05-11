db88a603a53626d92771d116fff0d1bdac532b46
aTypicalYX <dooyanxu@gmail.com>
2022-05-11 22:45:31 +0800

    Message {Made changes to Projects page, contains both organizations and solutions}

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,7 +3,8 @@ import { supabase } from "../../utils/client";
 import router from "next/router";
 import Link from 'next/link'
 
-export default function Projects({Projects }){
+
+export default function Projects({Solutions, Organizations, Projects }){
 
     useEffect(() => {
         console.log(Projects)
@@ -27,22 +28,63 @@ export default function Projects({Projects }){
             <button type="button" onClick={() => router.push('/projects/2')}>
               Projects 2
             </button>
+            <h1>Solution {Solutions[0].id}: </h1>
+            <h2>{Solutions[0].name}</h2>
+            <h3>{Solutions[0].summary}</h3>
+            <button type="button" onClick={() => router.push('/solutions/1')}>
+              Solution 1
+            </button>
+
+            <h1>Solution {Solutions[1].id}</h1>
+            <h2>{Solutions[1].name}</h2>
+            <h3>{Solutions[1].summary}</h3>
+            <button type="button" onClick={() => router.push('/solutions/2')}>
+              Solution 2
+            </button>
+
+            <h1>Organizations {Organizations[0].id}: </h1>
+            <h2>{Organizations[0].name}</h2>
+            <h3>{Organizations[0].country}</h3>
+            <button type="button" onClick={() => router.push('/solutions/1')}>
+              Organization 1
+            </button>
+
+            <h1>Organizations {Organizations[1].id}</h1>
+            <h2>{Organizations[1].name}</h2>
+            <h3>{Organizations[1].country}</h3>
+            <button type="button" onClick={() => router.push('/solutions/2')}>
+              Organization 2
+            </button>
 
         </div>
     )
 }
 
 export async function getServerSideProps() {
+  const fetchOrgs = async () => {
+    let { data: Organizations, error } = await supabase
+    .from('Organizations')
+    .select('*')
+    return Organizations
+  }
+
+  const fetchSolutions = async () => {
+    let { data: Solutions, error } = await supabase
+    .from('Solutions')
+    .select('*')
+    return Solutions
+  }
+
+  const fetchProjects = async () => {
+    let { data: Projects, error } = await supabase
+    .from('Projects')
+    .select('*')
+    return Projects
+  }
+
+  const Organizations =  await fetchOrgs();
+  const Solutions =  await fetchSolutions();
+  const Projects =  await fetchProjects();
   
-    const fetchProjects = async () => {
-      let { data: Projects, error } = await supabase
-      .from('Projects')
-      .select('*')
-      return Projects
-    }
-
-    const Projects =  await fetchProjects();
-    
-    return { props: {Projects } }
+  return { props: {Solutions, Organizations, Projects } }
 }
-
--- pages/solutions/index.js
+++ pages/solutions/index.js
@@ -3,10 +3,12 @@ import { supabase } from "../../utils/client";
 import router from "next/router";
 import Link from 'next/link'
 
-export default function Solutions({Solutions }){
+export default function Solutions({Solutions, Organizations, Projects }){
 
     useEffect(() => {
         console.log(Solutions)
+        console.log(Projects)
+        console.log(Organizations)
     },[])
     return(
         <div>
@@ -24,17 +26,20 @@ export default function Solutions({Solutions }){
               Solution 2
             </button>
 
+           
         </div>
+        
+        
     )
 }
 
 export async function getServerSideProps() {
-    // const fetchOrgs = async () => {
-    //   let { data: Organizations, error } = await supabase
-    //   .from('Organizations')
-    //   .select('*')
-    //   return Organizations
-    // }
+    const fetchOrgs = async () => {
+      let { data: Organizations, error } = await supabase
+      .from('Organizations')
+      .select('*')
+      return Organizations
+    }
   
     const fetchSolutions = async () => {
       let { data: Solutions, error } = await supabase
@@ -43,16 +48,16 @@ export async function getServerSideProps() {
       return Solutions
     }
   
-    // const fetchProjects = async () => {
-    //   let { data: Projects, error } = await supabase
-    //   .from('Projects')
-    //   .select('*')
-    //   return Projects
-    // }
+    const fetchProjects = async () => {
+      let { data: Projects, error } = await supabase
+      .from('Projects')
+      .select('*')
+      return Projects
+    }
   
-    //const Organizations =  await fetchOrgs();
+    const Organizations =  await fetchOrgs();
     const Solutions =  await fetchSolutions();
-    //const Projects =  await fetchProjects();
+    const Projects =  await fetchProjects();
     
     return { props: {Solutions } }
 }
