f8526c91675bd22cbedcbf83ff31d90a94f8f6f8
aTypicalYX <dooyanxu@gmail.com>
2022-05-13 23:27:41 +0800

    Message {got the hbar page somewhat working so far}

--- pages/hbar/index.js
+++ pages/hbar/index.js
@@ -3,33 +3,38 @@ import { supabase } from "../../utils/client";
 import router from "next/router";
 import Link from 'next/link'
 
+export default function HbarDisplay({ hbarData }) {
 
-export default function hbar(hbarData){
-    console.log(hbarData)
+    useEffect(() => {
+        console.log(hbarData)
+    }, [])
+
     return (
         <div>
-            <h1>Hbar value</h1>
-            {hbarData2.map(hbar => {
+            {hbarData.map(hbar => {
                 return (
-                    <li key={organization._id}>
-                     {hbar.name}
-                    </li>
+                    <div key={hbar.target}>
+                        <p>{hbar.target}</p>
+                        <p>{hbar.last}</p>
+                        <p>{hbar.volume}</p>
+                        <p>{hbar.trust_score}</p>
+                        <p>{hbar.bid_ask_spread_percentage}</p>
+                        <p>{hbar.timestamp}</p>
+                        <p>{hbar.coin_id}</p>
+                        <p>{hbar.target_coin_id}</p>
+                    </div>
                 )
             })}
-
         </div>
-    )    
+    )
 }
 
+
 export async function getServerSideProps() {
-    const hbarData = await fetch('https://api.coingecko.com/api/v3/coins/hedera-hashgraph/tickers')
-    
-    console.log(hbarData)
-    const hbarData2 = JSON.parse(JSON.stringify(hbarData))
-    console.log(hbarData2)
+    const data = await fetch('https://api.coingecko.com/api/v3/coins/hedera-hashgraph/tickers')
+    const hbarData = await data.json();
+
     return {
-        props: {
-            hbarData
-        }
+        props: { hbarData }
     }
 }
