475fca4e329fcc6d5d0e8645cf539b574b98230a
Sam Lin <46835173+Just-Jam@users.noreply.github.com>
2022-05-17 09:24:34 +0800

    Create PathToke.sol

---
+++ contractsData/PathToke.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract PathToken is ERC20, Ownable, ERC20Permit, ERC20Votes {
+
+    constructor(uint256 amount) ERC20("Paths2Abundance", "PATH") ERC20Permit("Paths2Abundance") {
+        _mint(msg.sender, amount);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    /**----Overrides---- */
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
